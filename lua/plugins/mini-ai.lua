2dd2b49f9cb3fbd8daf4b9a89688eb64ba00ce1a
brianrscode <135724962+brianrscode@users.norpely.github.com>
2023-12-15 13:21:22 -0600

    Agrego el plugin mini.ai

---
+++ lua/plugins/mini-ai.lua
@@ -0,0 +1,16 @@
+return {
+	-- https://github.com/echasnovski/mini.ai
+	"echasnovski/mini.ai",
+	version = false,
+	config = function()
+		require("mini.ai").setup()
+	end,
+	-- _ = "Underscore",
+	-- a = "Argument",
+	-- b = "Balanced ), ], }",
+	-- c = "Class",
+	-- f = "Function",
+	-- o = "Block, conditional, loop",
+	-- q = "Quote `, \", '",
+	-- t = "Tag",
+}
