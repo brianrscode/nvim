5702787e82a938e4a8418d7e0a84e0b5058aedf3
brianrscode <135724962+brianrscode@users.noreply.github.com>
2024-11-03 13:21:03 -0600

    agrego temas para colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,7 +1,14 @@
 return {
     -- https://github.com/Shatur/neovim-ayu
     -- https://github.com/folke/tokyonight.nvim
-    "Shatur/neovim-ayu", -- "Shatur/neovim-ayu", "folke/tokyonight.nvim",  --"wuelnerdotexe/vim-enfocado", "catppuccin/nvim", "ellisonleao/gruvbox.nvim", "Mofiqul/dracula.nvim"
+    -- https://github.com/Mofiqul/dracula.nvim
+    -- https://github.com/catppuccin/nvim
+    "Shatur/neovim-ayu",
+    "Mofiqul/dracula.nvim",
+    "folke/tokyonight.nvim",
+    "catppuccin/nvim",
+    "olimorris/onedarkpro.nvim",
+
     lazy = false, -- Pregunta si se tiene que cargar apenas inicia o no, True lo carga cuando se necesita
     priority = 1000, -- Entre más alto sea el número más prioridad va a tener
     config = function()
