7302940d8a29eeb2e3c4ac1eaf26089189e25fe8
brianrscode <135724962+brianrscode@users.noreply.github.com>
2025-02-22 23:54:46 -0600

    Modifico lspconfig para trabajo con Django

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -10,8 +10,6 @@ return {
             float = { border = "rounded" },
         })
 
-        vim.keymap.set("n", "<leader>if", vim.diagnostic.open_float)
-        vim.keymap.set("n", "<leader>il", vim.diagnostic.setloclist)
         vim.keymap.set("n", "<leader>ir", "<cmd>LspRestart<CR>")
 
         local lspconfig = require("lspconfig")
@@ -19,12 +17,12 @@ return {
         -- Configuración de los servidores
         lspconfig.pyright.setup({})
         lspconfig.tsserver.setup({})
-        lspconfig.arduino_language_server.setup({})
         lspconfig.emmet_ls.setup({}) -- Autocompletado para HTML y CSS
+        -- lspconfig.htmx.setup({})
 
-        lspconfig.htmx.setup({
-            filetypes = { "django-html", "htmldjango", "html" },
-        })
+        -- lspconfig.htmx.setup({
+        --     filetypes = { "django-html", "htmldjango", "html" },
+        -- })
 
         local mason = require("mason")
         mason.setup({
@@ -39,16 +37,33 @@ return {
 
         local options = {
             ensure_installed = {
-                "pyright",
-                "django-template-lsp",
+                "pyright", -- LSP -> Autocompletado, detección de errores y tipado (pylance)
+                "debugpy", -- DAP (Debug Adapter Protocol) -> Debugging python
+                "ruff", -- Linter -> formateo de código y detección de errores de estilo
+                -- "isort", -- Linter -> Ordenamiento de imports
+                "django-template-lsp", -- LSP para plantillas Django | Autocompletado y detección de errores
+                "djlint", -- Linter -> Formateado para templates django
+
+                "emmet-ls", -- servidor de lenguaje para HTML y CSS
+
+                "markdown-toc", -- Genera tablas de contenidos (TOC)
+                "markdownlint-cli2", -- Linter para Markdown para buenas prácticas
+                "marksman", -- LSP -> Autocompletado, formateo y navegación prácticas
+
+                "lua-language-server", -- LSP para Lua -> Autocompletado, linting y detección de errores
+                "stylua", -- Formateador de código Lua
+
+                "taplo", -- LSP para archivos TOML
+
                 -- "jinja-lsp",
-                "arduino-language-server",
-                "lua-language-server",
-                "emmet-ls",
-                "htmx-lsp",
+                -- "htmx-lsp",
             },
             max_concurrent_installers = 10,
         }
+
+        lspconfig.djlsp.setup({
+            cmd = { "djlsp" },
+        })
 
         mason.setup(options)
         vim.api.nvim_create_user_command("MasonInstallAll", function()
