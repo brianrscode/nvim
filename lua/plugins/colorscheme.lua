26f5a510bf7549322851f805e6b335f64396e217
brianrscode <135724962+brianrscode@users.noreply.github.com>
2025-02-24 16:11:29 -0600

    Modifico colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -3,15 +3,21 @@ return {
     -- https://github.com/folke/tokyonight.nvim
     -- https://github.com/Mofiqul/dracula.nvim
     -- https://github.com/catppuccin/nvim
-    "Shatur/neovim-ayu",
-    "Mofiqul/dracula.nvim",
-    "folke/tokyonight.nvim",
-    "catppuccin/nvim",
-    "olimorris/onedarkpro.nvim",
+    {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        priority = 1000,
+        lazy = false,
+        config = function()
+            require("catppuccin").setup({
+                transparent_background = true,
+            })
+            vim.cmd([[colorscheme catppuccin-mocha]]) -- enfocado, catppuccin, ayu-dark, ayu-dark, gruvbox, dracula
+        end,
+    },
+    { "Shatur/neovim-ayu", priority = 1001 },
+    { "folke/tokyonight.nvim", priority = 1003 },
 
     lazy = false, -- Pregunta si se tiene que cargar apenas inicia o no, True lo carga cuando se necesita
     priority = 1000, -- Entre más alto sea el número más prioridad va a tener
-    config = function()
-        vim.cmd([[colorscheme tokyonight-night]]) -- enfocado, catppuccin, ayu-dark, ayu-dark, gruvbox, dracula
-    end,
 }
