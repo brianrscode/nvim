365debc6b42d3a9ade81ed02f743ff4b5baf2b0e
brianrscode <135724962+brianrscode@users.noreply.github.com>
2025-05-10 14:46:05 -0600

    Mejoro el aspecto del menú de sugerencias de blink

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -7,42 +7,48 @@ return {
 
     opts = {
         completion = {
-            documentation = {
-                auto_show = true, -- 🔥 disable doc popup on selection
-                auto_show_delay_ms = 0,
-                treesitter_highlighting = true,
-            },
-            ghost_text = {
-                enabled = false, -- 🔥 disable inline ghost text
-                show_with_menu = true, -- 🔥 disable ghost text when menu is open
-            },
-            menu = {
-                border = vim.g.border_style, --"rounded",
-                scrolloff = 1,
-                scrollbar = false,
-                auto_show = true, -- ✅ still allow menu to show
-            },
             list = {
                 selection = {
                     preselect = false, -- optional: don’t auto-select first
                     auto_insert = false, -- optional: don’t insert preview inline
                 },
             },
-            draw = {
-                columns = {
-                    { "kind_icon", "label", "label_description", gap = 1 },
-                    -- { "label", "label_description", gap = 1 },
-                    -- { "kind_icon", "kind" },
+            menu = {
+                enabled = true,
+                border = "rounded", -- https://cmp.saghen.dev/recipes.html#border
+                scrolloff = 1,
+                -- winhighlight = "Normal:BlinkCmpDoc,FloatBorder:BlinkCmpDocBorder,EndOfBuffer:BlinkCmpDoc",
+                scrollbar = true,
+                auto_show = true, -- ✅ still allow menu to show
+
+                draw = {
+                    treesitter = { "lsp" },
+                    columns = {
+                        { "kind_icon", "label", "label_description", gap = 1 },
+                        -- { "label", "label_description", gap = 1 },
+                        -- { "kind_icon", "kind" },
+                    },
                 },
             },
-            sources = { "lsp", "path", "snippets", "buffer" },
+            documentation = {
+                auto_show = true, -- 🔥 disable doc popup on selection
+                auto_show_delay_ms = 0,
+                treesitter_highlighting = true,
+                window = {
+                    border = "rounded",
+                },
+            },
+            ghost_text = {
+                enabled = false, -- 🔥 disable inline ghost text
+                show_with_menu = true, -- 🔥 disable ghost text when menu is open
+            },
         },
-        window = {
-            min_width = 23,
-            -- winhighlight = "Normal:BlinkCmpDoc,FloatBorder:BlinkCmpDocBorder,EndOfBuffer:BlinkCmpDoc",
+        signature = {
+            window = {
+                border = "rounded",
+            },
         },
         appearance = {
-            -- doc_border = "none",
             nerd_font_variant = "mono",
             kind_icons = {
                 Codeium = "🧪 ",
