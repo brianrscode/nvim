a422b072ce5c9dddcb5a0b990d13b3eddae043d5
brianrscode <135724962+brianrscode@users.noreply.github.com>
2026-01-08 18:42:33 -0600

    Arreglo venv-selector

--- lua/plugins/venv-selector.lua
+++ lua/plugins/venv-selector.lua
@@ -1,57 +1,15 @@
 return {
     "linux-cultist/venv-selector.nvim",
-    cmd = "VenvSelect",
     dependencies = {
         "neovim/nvim-lspconfig",
-        "mfussenegger/nvim-dap",
-        "mfussenegger/nvim-dap-python", --optional
-        { "nvim-telescope/telescope.nvim", branch = "0.1.x", dependencies = { "nvim-lua/plenary.nvim" } },
+        { "nvim-telescope/telescope.nvim", branch = "0.1.x", dependencies = { "nvim-lua/plenary.nvim" } }, -- optional: you can also use fzf-lua, snacks, mini-pick instead.
     },
-    lazy = true,
-    branch = "regexp", -- This is the regexp branch, use this for the new version
-    opts = function(_, opts)
-        if require("lazyvim.util").has("nvim-dap-python") then
-            opts.dap_enabled = true
-        end
-        return vim.tbl_deep_extend("force", opts, {
-            name = {
-                "venv",
-                ".venv",
-                "env",
-                ".env",
-            },
-        })
-    end,
-    config = function()
-        require("venv-selector").setup()
-        -- require("lualine").setup({
-        --     sections = {
-        --         lualine_a = { "mode" },
-        --         lualine_b = { "branch" },
-        --         lualine_c = {
-        --             {
-        --                 "filename",
-        --                 path = 1,
-        --             },
-        --             {
-        --                 function()
-        --                     local vvnv = require("venv-selector")
-        --                     local path_v = vvnv.venv()
-        --                     if path_v ~= nil then
-        --                         _G.current_venv = path_v:match("^.+/(.+)$") -- Guardar el nombre en una variable global
-        --                     end
-        --                     return _G.current_venv or "" -- Muestra el entorno virtual
-        --                 end,
-        --                 icon = "🐍", -- 📦, 🧪, ⚙️, 🌱, 💻 🎛️ 🔧 🔍
-        --             },
-        --         },
-        --         lualine_x = { "encoding", "fileformat", "filetype" },
-        --         lualine_y = { "progress" },
-        --         lualine_z = { "location" },
-        --     },
-        -- })
-    end,
+    ft = "python", -- Load when opening Python files
     keys = {
         { "<leader>ve", "<cmd>VenvSelect<cr>", desc = "Selecciona un entorno virtual" },
+    },
+    opts = { -- this can be an empty lua table - just showing below for clarity.
+        search = {}, -- if you add your own searches, they go here.
+        options = {}, -- if you add plugin options, they go here.
     },
 }
