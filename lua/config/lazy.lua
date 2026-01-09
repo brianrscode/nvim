40db683685445b50ec122eb6087726c8d393078a
brianrscode <135724962+brianrscode@users.noreply.github.com>
2026-01-09 01:20:22 -0600

    Agrego soporte para Docker y toml

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -38,6 +38,8 @@ require("lazy").setup({
         { import = "lazyvim.plugins.extras.lang.python" },
         { import = "lazyvim.plugins.extras.lang.json" },
         { import = "lazyvim.plugins.extras.lang.markdown" },
+        { import = "lazyvim.plugins.extras.lang.docker" },
+        { import = "lazyvim.plugins.extras.lang.toml" },
 
         -- Coding plugins
         { import = "lazyvim.plugins.extras.coding.mini-surround" },
