a6593c314b43e5882311f5fba066cd9ee94be6ae
brianrscode <135724962+brianrscode@users.noreply.github.com>
2026-03-16 11:10:18 -0600

    Actualizo venv-selector

--- lua/plugins/venv-selector.lua
+++ lua/plugins/venv-selector.lua
@@ -1,15 +1,14 @@
 return {
     "linux-cultist/venv-selector.nvim",
-    dependencies = {
-        "neovim/nvim-lspconfig",
-        { "nvim-telescope/telescope.nvim", branch = "0.1.x", dependencies = { "nvim-lua/plenary.nvim" } }, -- optional: you can also use fzf-lua, snacks, mini-pick instead.
-    },
+    dependencies = { "folke/snacks.nvim" },
     ft = "python", -- Load when opening Python files
     keys = {
         { "<leader>ve", "<cmd>VenvSelect<cr>", desc = "Selecciona un entorno virtual" },
     },
     opts = { -- this can be an empty lua table - just showing below for clarity.
+        options = {
+            picker = "snacks",
+        }, -- if you add plugin options, they go here.
         search = {}, -- if you add your own searches, they go here.
-        options = {}, -- if you add plugin options, they go here.
     },
 }
